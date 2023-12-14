65155022ea201c0be9b62a98bfeacd037dc34ab9
d-atienza <153103146+d-atienza@users.noreply.github.com>
2023-12-14 14:27:51 +0000

    other js files added in

---
+++ js/createSnowFall.js
@@ -0,0 +1,11 @@
+function snowFall() {
+	createSnow();
+}
+
+function createSnow() {
+	for (let i = 0; i < 100; i++) {
+		fill(255, 255, 255, random(50, 255));
+		circle(random(10, 790), random(10, 400), random(5, 15));
+	}
+}
+
